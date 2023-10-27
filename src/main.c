b93b8022a6d7c9475c577ec49543b2dd41bd98e0
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2023-10-26 17:54:20 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -333,9 +333,8 @@ void MkDir(const char *s) {
 #endif
 }
 
-#define UNUSED(var) var __attribute__((unused))
 #if defined (_WIN32)
-int WINAPI WinMain(UNUSED(HINSTANCE hInstance), UNUSED(HINSTANCE hPrevInstance), UNUSED(PSTR szCmdLine), UNUSED(int iCmdShow)) {
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
   int argc = 0;
   uTCHAR **argv = CommandLineToArgvW(GetCommandLineW(), &argc);
 #else
